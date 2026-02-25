0e9eedbc05e39860b5ca164b5ff286bf0aadd1e1
kriztpow <45737457+kriztpow@users.noreply.github.com>
2026-02-25 18:31:56 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.tvbrowser"
+    namespace = "com.example.remoteclient"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.tvbrowser"
+        applicationId = "com.example.remoteclient"
         minSdk = 30
         targetSdk = 34
         versionCode = 1
